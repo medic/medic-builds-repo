324416f43153430538f4eaf3cfce0ad223ace817
Stefan du Fresne <stefan@sdufresne.info>
2017-06-23 19:08:24 +0200

    Fixing beta and branch regex

--- ddocs/builds/views/betas/map.js
+++ ddocs/builds/views/betas/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  var semver = doc._id.match(/^(\d+)\.(\d+)\.(\d+)-beta\.{\d+}$/);
+  var semver = doc._id.match(/^(\d+)\.(\d+)\.(\d+)-beta\.(\d+)$/);
   if (semver) {
     emit([parseInt(semver[1]), parseInt(semver[2]), parseInt(semver[3]), parseInt(semver[4])], {
       build_time: doc.kanso && doc.kanso.build_time
--- ddocs/builds/views/branches/map.js
+++ ddocs/builds/views/branches/map.js
@@ -1,5 +1,5 @@
 function(doc) {
-  var semver = doc._id.match(/^(\d+)\.(\d+)\.(\d+)(-beta\.{\d+})?$/);
+  var semver = doc._id.match(/^\d+\.\d+\.\d+(-beta\.\d+)?$/);
   if (doc._id.indexOf('_design/') === 0 || !semver) {
     emit(doc._id, {
       build_time: doc.kanso && doc.kanso.build_time
