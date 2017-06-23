5508964747aeca0a5a57feae765b1f2d9615fa79
Stefan du Fresne <stefan@sdufresne.info>
2017-06-23 19:02:03 +0200

    versions are ints now

--- ddocs/builds/views/betas/map.js
+++ ddocs/builds/views/betas/map.js
@@ -1,7 +1,7 @@
 function(doc) {
   var semver = doc._id.match(/^(\d+)\.(\d+)\.(\d+)-beta\.{\d+}$/);
   if (semver) {
-    emit([semver[1], semver[2], semver[3], semver[4]], {
+    emit([parseInt(semver[1]), parseInt(semver[2]), parseInt(semver[3]), parseInt(semver[4])], {
       build_time: doc.kanso && doc.kanso.build_time
     });
   }
