16ec7b5e1d51dd4a5ce2a62441935e7e08a33d46
Stefan du Fresne <stefan@sdufresne.info>
2018-06-01 16:57:34 +0100

    Supporting wipe

--- src/index.js
+++ src/index.js
@@ -7,29 +7,41 @@ const PouchDB = require('pouchdb-core')
 
 const DDOC_PATH = path.join(__dirname, '..', 'ddocs', 'builds');
 
-const init = (url, { wipe } = {}) => {
+const db = (url, wipe) => {
   const DB = new PouchDB(url);
 
-  return new Promise((resolve, reject) =>
-    compile(DDOC_PATH, (err, ddoc) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(ddoc);
-      }
-  }))
-  .then(ddoc => {
-    return DB.get(ddoc._id)
-      .catch(err => {
-        if (err.status !== 404) {
-          throw err;
-        }
-      })
-      .then((existing={}) => {
-        ddoc._rev = existing._rev;
-      })
-      .then(() => DB.put(ddoc));
-  });
+  if (wipe) {
+    return DB.destroy()
+      .then(() => new PouchDB(url));
+  } else {
+    return DB;
+  }
+};
+
+const init = (url, { wipe } = {}) => {
+  return db(url, wipe)
+    .then(() => {
+      return new Promise((resolve, reject) =>
+        compile(DDOC_PATH, (err, ddoc) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(ddoc);
+          }
+      }))
+      .then(ddoc => {
+        return DB.get(ddoc._id)
+          .catch(err => {
+            if (err.status !== 404) {
+              throw err;
+            }
+          })
+          .then((existing={}) => {
+            ddoc._rev = existing._rev;
+          })
+          .then(() => DB.put(ddoc));
+      });
+    });
 };
 
 module.exports = {
