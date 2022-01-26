3803b0a0422fd60e8e9e3b0c8866332326379eea
Diana Barsan <barsan@medicmobile.org>
2022-01-26 10:18:16 +0200

    No DB array.

--- scripts/pushToServer.js
+++ scripts/pushToServer.js
@@ -4,20 +4,11 @@ const PouchDB = require('pouchdb-core').plugin(require('pouchdb-adapter-http'));
 
 const compile = async (buildPath) => promisify(require('couchdb-compile'))(buildPath);
 
-const DBS = [
-  // Production
-  'builds',
-
-  // Testing by confirmed employees. Code in this repo gets moved to `builds`
-  // when it wants to be published (e.g. it's a tag)
-  'builds_testing',
-
-  // specifically NOT pushing to this one. This server should not use the build server code as we
-  // don't want the validation: here anonymous users *are* allowed to create builds. This is OK
-  // as these builds are only used for their external PRs and will never directly make it into
-  // production
-  // 'builds_external'
-];
+const PRODUCTION_DB = 'builds';
+// Testing by confirmed employees. Code in this repo gets moved to `builds` after e2e tests are successful.
+const TESTING_DB = 'builds_testing';
+// Builds by external contributors. Code in this repo never gets moved to `builds`.
+const EXTERNAL_DB = 'builds_external';
 
 const pushDdoc = async (dbName, compiledDdoc) => {
   console.log(`:: pushing ddoc to ${dbName}`);
@@ -40,16 +31,17 @@ const pushDdoc = async (dbName, compiledDdoc) => {
   console.log('..done');
 };
 
-(async function main(){
+(async function main() {
   console.log('Compiling ddocs');
   const ddoc = await compile(path.join(__dirname, '..', 'ddocs/builds'));
   const buildsExternalDdoc = await compile(path.join(__dirname, '..', 'ddocs/builds_external'));
 
-  for (const dbName of DBS) {
-    await pushDdoc(dbName, ddoc);
-  }
+  await pushDdoc(PRODUCTION_DB, ddoc);
 
-  await pushDdoc('builds_external', buildsExternalDdoc);
+  await pushDdoc(TESTING_DB, ddoc);
+
+  await pushDdoc(EXTERNAL_DB, buildsExternalDdoc);
+
 })().catch(err => {
   console.error(err);
   process.exit(-1);
