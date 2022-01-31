cd2ebaffe83b1f7533ddfac6e078f7388764bc5d
Diana Barsan <barsan@medicmobile.org>
2022-01-31 19:34:24 +0200

    we should never have anything but branches here!

--- ddocs/builds_external/views/releases/map.js
+++ ddocs/builds_external/views/releases/map.js
@@ -5,10 +5,7 @@ function(doc) {
         { time: doc.kanso && doc.kanso.build_time } :
         doc.build_info;
 
-      if (version.branch) {
-        // Branch, medic:medic:my-cool-branch
-        emit(['branch', version.namespace, version.application, meta.time, version.branch], meta);
-      }
+    emit(['branch', version.namespace, version.application, meta.time, version.branch], meta);
   } else {
     log('The document ' + doc._id + ' cannot be matched against correctly');
   }
